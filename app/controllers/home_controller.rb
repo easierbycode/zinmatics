be973e5a7459b61c98cdc558ef56d5cddcd7637d
Daniel Johnson <navseeker7@gmail.com>
2013-04-04 00:51:51 -0700

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+  def learn_more
+  end
+
+  def sign_in
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zinmatics::Application.config.secret_key_base = '494e345152ac07774fe455b1bead3529c48f7fda0f1b835e9e80491523ada8c35b9feb41cb7f7a21f8b6a097f9aedb898229f2192b5d7a1ae7b891ae0605a30a'
